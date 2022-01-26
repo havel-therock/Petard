238f46029568299fa90fbb7fd392923c69e58d66
Kacper <kacper.szatan@gmail.com>
2022-01-26 20:28:32 +0100

    premake: add basic projects generator
    Premake is used to generates projects.
    Also bat file that run premake is added.
    Premake contains post Petard build step that moves dll
    in to place where Sandbox.exe is created

---
+++ premake5.lua
@@ -0,0 +1,111 @@
+workspace "Petard"
+	architecture "x64"
+
+	configurations
+	{
+		"Debug",
+		"Release",
+		"Dist"
+	}
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+project "Petard"
+	location "Petard"
+	kind "SharedLib"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+	
+	pchheader "pdpch.h"
+	pchsource "Petard/src/pdpch.cpp"
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs
+	{
+		"%{prj.name}/src",
+		"%{prj.name}/vendor/spdlog/include"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "latest"
+
+		defines
+		{
+			"PD_PLATFORM_WINDOWS",
+			"PD_BUILD_DLL"
+		}
+
+		--- change this in to prebuild comend before every gameproject
+
+		postbuildcommands
+		{
+			("{COPY} %{cfg.buildtarget.relpath} ../bin/" .. outputdir .. "/Sandbox")
+		}
+
+	filter "configurations:Debug"
+		defines "PD_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "PD_RELEASE"
+		optimize "On"
+
+	filter "configurations:Dist"
+		defines "PD_DIST"
+		optimize "On"
+
+project "Sandbox"
+	location "Sandbox"
+	kind "ConsoleApp"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+	
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs
+	{
+		"Petard/vendor/spdlog/include",
+		"Petard/src"
+	}
+
+	links
+	{
+		"Petard"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "latest"
+
+		defines
+		{
+			"PD_PLATFORM_WINDOWS"
+		}
+
+	filter "configurations:Debug"
+		defines "PD_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "PD_RELEASE"
+		optimize "On"
+
+	filter "configurations:Dist"
+		defines "PD_DIST"
+		optimize "On"
